c0b0a6b9ac0d14c8a23118f05494f8cf0dfb2f85
Juliano Rubio Sangaleti <julianosangaleti@alunos.utfpr.edu.br>
2025-11-20 19:44:25 -0300

    Formatação do arquivo
    Coloquei todas as bibliotecas que estavam sendo puxadas no arquivo cpp para o header e formatei

--- include/index.hpp
+++ include/index.hpp
@@ -4,33 +4,34 @@
 #include <unordered_map>
 #include <vector>
 #include <string>
+#include <algorithm>
 
-class Index {
-    public:
-        Index();
+class Index
+{
+public:
+    Index();
 
-        // Adiciona uma plavra associada a um arquivo (por ID)
-        void addWord(const std::string &word, int documentId);
+    // Adiciona uma plavra associada a um arquivo (por ID)
+    void addWord(const std::string &word, int documentId);
 
-        // Obtém a lista de documentos que contém a palavra
-        std::vector<int> getDocuments(const std::string &word) const;
+    // Obtém a lista de documentos que contém a palavra
+    std::vector<int> getDocuments(const std::string &word) const;
 
-        // Registra um arquivo novo e retorna seu ID
-        int registerDocument(const std::string &fileName);
+    // Registra um arquivo novo e retorna seu ID
+    int registerDocument(const std::string &fileName);
 
-        // Obtém nome do arquivo a partid de seu ID
-        std::string getFileName(int id) const;
+    // Obtém nome do arquivo a partid de seu ID
+    std::string getFileName(int id) const;
 
-    private: 
-        // palavra -> lista de IDs de documentos
-        std::unordered_map<std::string, std::vector<int>> invertedIndex;
+private:
+    // palavra -> lista de IDs de documentos
+    std::unordered_map<std::string, std::vector<int>> invertedIndex;
 
-        // documentoId-> nome do arquivo
-        std::vector<std::string> idToFileName;
+    // documentoId-> nome do arquivo
+    std::vector<std::string> idToFileName;
 
-        // nome do arquivo -> id
-        std::unordered_map<std::string, int> fileNameToId;
-
+    // nome do arquivo -> id
+    std::unordered_map<std::string, int> fileNameToId;
 };
 
 #endif
